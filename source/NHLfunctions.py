06a1c297408946d529d1623a8e05e78eb9994f57
getsheked <paulallenburg@gmail.com>
2025-12-04 12:27:02 -0600

    updated test code

--- source/NHLfunctions.py
+++ source/NHLfunctions.py
@@ -1,11 +1,11 @@
 import json
 import requests
 import datetime
-#import RPi.GPIO as GPIO
-#from adafruit_ht16k33 import segments
+import RPi.GPIO as GPIO
+from adafruit_ht16k33 import segments
 import time
-#import board
-#import busio
+import board
+import busio
 from configparser import ConfigParser
 
 #config setup
