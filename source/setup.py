2c9610db604ebfbec942cfd630ca0432e1eab196
getsheked <paulallenburg@gmail.com>
2025-12-04 13:05:32 -0600

    regorznaiging files

---
+++ source/setup.py
@@ -0,0 +1,19 @@
+import board
+import busio
+import datetime
+import configparser 
+from adafruit_ht16k33 import segments
+class board:
+    i2c = busio.I2C(board.SCL, board.SDA)
+    config=ConfigParser()
+    config.read('config.ini')
+    today = datetime.date.today()
+    date1=today.strftime("%Y-%m-%d")
+    abrev=config.get('team','teamABV')
+    teamID=int(config.get('team','teamID'))
+    tzone=config.get('time','zone')
+    timeFormat=int(config.get('time','24hr'))
+    season=config.get('time','season')
+    display2=segments.Seg7x4(i2c, address=0x74)
+    display=segments.Seg14x4(i2c,address =0x73)
+print(board.today)
